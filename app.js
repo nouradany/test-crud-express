eb0a2691d395e491591ba33566bdb810e410967a
lamia <nouradany3@gmail.com>
2024-03-09 18:00:51 +0100

    add get api

--- app.js
+++ app.js
@@ -12,19 +12,8 @@ app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
 
-app.post("/", (req, res) => {
-    console.log("GET request successful");
-    res.send("Get Req Success!");
-});
 
 
-app.put("/", (req, res) => {
-    console.log("GET request successful");
-    res.send("Get Req Success!");
-});
 
-app.delete("/", (req, res) => {
-    console.log("GET request successful");
-    res.send("Get Req Success!");
-});
+
 
